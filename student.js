cd1a47792a514a4704006e6eef5976c42f075ab7
aknauft <aknauft@gmail.com>
2018-02-23 00:38:09 -0800

    Better default ranges

--- student.js
+++ student.js
@@ -1,12 +1,12 @@
 const w = 640;  const h = 480;
-let realRange = [-0.5, 0.5];  let imagRange = [-0.5, 0.5];
+let realRange = [-0.1, 0.9];  let imagRange = [-0.1, 0.9];
 
 function FancyComplexMath(complex) {
   let a = complex.real;
   let b = complex.imaginary;
 
   return {
-    real: a+0.25, // a*a-b*b,
-    imaginary: b-0.25 // 2*a*b;
+    real: (a*a-b*b), //a*a-b*b,
+    imaginary: 2*a*b, //2*a*b
   }
 }
